6c4208cdc902baba29cd789939d53f8490944642
hot666666 <hotcs6@naver.com>
2024-10-18 13:49:35 +0900

    feat : add utils - argmax func

---
+++ common/utils.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def argmax(xs):
+    idxes = [i for i, x in enumerate(xs) if x == max(xs)]
+    if len(idxes) == 1:
+        return idxes[0]
+    elif len(idxes) == 0:
+        return np.random.choice(len(xs))
+
+    selected = np.random.choice(idxes)
+    return selected
